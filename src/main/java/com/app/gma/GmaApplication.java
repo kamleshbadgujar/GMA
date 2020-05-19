d671a51a809053ecc31629df1881320c936d1b54
Kamlesh Badgujar <kamleshbadgujar00@gmail.com>
2020-05-19 22:25:07 +0530

    19 May 2020-initial code added

---
+++ src/main/java/com/app/gma/GmaApplication.java
@@ -0,0 +1,13 @@
+package com.app.gma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/gm/GmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
